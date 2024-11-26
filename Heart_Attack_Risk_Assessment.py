90abe0e730dbe7dcdc4fefae1615a52336fd3b4f
inhaiding <wtemitope308@gmail.com>
2024-11-26 18:02:50 +0100

    Add files via upload

--- Heart_Attack_Risk_Assessment.py
+++ Heart_Attack_Risk_Assessment.py
@@ -14,11 +14,11 @@ st.write("This app predicts the risk of heart attack/disease based on treatment.
 # Input fields for user to enter feature values
 Gender = st.number_input('Gender (GENDER)', min_value=0.0, max_value=2.0, value=10.0, step=1.0)
 Age = st.number_input('Age (AGE)', min_value=1.0, max_value=100.0, value= 100.0, step=0.1)
-Blood Pressure = st.number_input('Blood Pressure (mmHg) (BLOOD_PRESSURE (mmHg))', min_value=0.0, max_value=150.0,value = 10.0, step=0.1)
+Blood_pressure = st.number_input('Blood Pressure (mmHg) (BLOOD_PRESSURE (mmHg))', min_value=0.0, max_value=150.0,value = 10.0, step=0.1)
 Cholesterol  = st.number_input('Cholesterol (mg/dL) (CHOLESTEROL)', min_value=0.0, max_value=100.0,value = 100.0, step=0.1)
 Diabetes = st.number_input('Has Diabetes (DIABETES)', min_value=0.0, max_value=200.0,value = 200.0, step=1.0)
-Smoking Status = st.number_input('Smoking Status (SMOKING_STATUS)', min_value=0.0, max_value=10.0, value = 10.0, step=1.0)
-Chest Pain Type = st.number_input('Chest Pain Type (CHEST_PAIN_TYPE)', min_value=0.0, max_value=10.0, value=5.0, step=1.0)
+Smoking_status = st.number_input('Smoking Status (SMOKING_STATUS)', min_value=0.0, max_value=10.0, value = 10.0, step=1.0)
+Chest_pain_type = st.number_input('Chest Pain Type (CHEST_PAIN_TYPE)', min_value=0.0, max_value=10.0, value=5.0, step=1.0)
 
 # Create a dictionary with the input data
 input_data = {

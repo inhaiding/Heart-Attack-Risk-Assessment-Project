ed2aa30664513999c14504fc4229ff2ce1fbe84a
inhaiding <wtemitope308@gmail.com>
2024-11-29 23:02:02 +0100

    Add files via upload

--- Heart_Attack_Risk_Assessment.py
+++ Heart_Attack_Risk_Assessment.py
@@ -12,23 +12,38 @@ st.title('Heart Attack Risk Assessment')
 st.write("This app predicts the risk of heart attack/disease based on treatment.")
 
 # Input fields for user to enter feature values
-Gender = st.number_input('Gender (GENDER)', min_value=0.0, max_value=2.0, value=10.0, step=1.0)
+# Input field for Gender
+gender = st.selectbox('Select Gender:', options=['Male', 'Female', 'Other'])
+
+# Encoding the selected gender
+gender_mapping = {'Male': 0, 'Female': 1, 'Other': 2}
+gender_encoded = gender_mapping[gender]
+
 Age = st.number_input('Age (AGE)', min_value=1.0, max_value=100.0, value= 100.0, step=0.1)
-Blood_pressure = st.number_input('Blood Pressure (mmHg) (BLOOD_PRESSURE(mmHg))', min_value=0.0, max_value=150.0,value = 10.0, step=0.1)
+BloodPressure = st.number_input('Blood Pressure (mmHg) (BLOOD_PRESSURE(mmHg))', min_value=0.0, max_value=150.0,value = 10.0, step=0.1)
 Cholesterol  = st.number_input('Cholesterol (mg/dL) (CHOLESTEROL)', min_value=0.0, max_value=100.0,value = 100.0, step=0.1)
-Diabetes = st.number_input('Has Diabetes (DIABETES)', min_value=0.0, max_value=200.0,value = 200.0, step=1.0)
-Smoking_status = st.number_input('Smoking Status (SMOKING_STATUS)', min_value=0.0, max_value=10.0, value = 10.0, step=1.0)
-Chest_pain_type = st.number_input('Chest Pain Type (CHEST_PAIN_TYPE)', min_value=0.0, max_value=10.0, value=5.0, step=1.0)
+# Input field for Diabetes
+Diabetes = st.selectbox('Does the person have Diabetes?', options=['No', 'Yes'])
+
+# Map the selected option to a numeric value
+diabetes_mapping = {'No': 0, 'Yes': 1}
+diabetes_encoded = diabetes_mapping[diabetes]
+
+SmokingStatus = st.number_input('Smoking Status (SMOKING_STATUS)', min_value=0.0, max_value=10.0, value = 10.0, step=1.0)
+ChestPainType = st.number_input('Chest Pain Type (CHEST_PAIN_TYPE)', min_value=0.0, max_value=10.0, value=5.0, step=1.0)
+
+st.write(f"Encoded Gender: {gender_encoded}")
+st.write(f"Encoded Diabetes Value: {diabetes_encoded}")
 
 # Create a dictionary with the input data
 input_data = {
-    'GENDER':Gender,
-    'AGE':age,
-    'BLOOD_PRESSURE':Blood Pressure (mmHg),
-    'CHOLESTEROL':Cholesterol (mg/dL),
-    'DIABETES':Has Diabetes,
-    'SMOKING_STATUS':Smoking Status,
-    'CHEST_PAIN_TYPE':Chest Pain Type
+    'GENDER': Gender,
+    'AGE': age,
+    'BLOOD_PRESSURE': Blood Pressure (mmHg),
+    'CHOLESTEROL': Cholesterol (mg/dL),
+    'DIABETES': Has Diabetes,
+    'SMOKING_STATUS': Smoking Status,
+    'CHEST_PAIN_TYPE': Chest Pain Type
 }
 
 # Convert the dictionary to a DataFrame
